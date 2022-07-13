ee1aa5aa43fdfef173b4b81cf611a106cdb3adf8
cjayjoo <cjayjo@icloud.com>
2022-07-13 12:07:00 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simple Memorandum
+
+This is my first JavaScript project!!
+
+- [Note]
+- This allows you to easily add new memos.
+
+- [Date & Time]
+- Add date and time to your memo
+
+- [Add Note]
+- Added memo button
+
+- [Delete Note]
+- Delete memo button
---
+++ index.js
@@ -0,0 +1,39 @@
+const content = document.getElementById("content");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+const addedBtn = document.getElementById("addedBtn");
+const deletedBtn = document.getElementById("deletedBtn");
+const list = document.getElementById("list");
+
+const listContent = [];
+
+function render() {
+  let htmlStr = "";
+  listContent.forEach(function (item) {
+    htmlStr =
+      htmlStr +
+      `
+      <div class="item">
+          <div>
+            <p>Note：${item.content}</p>
+            <p>Date：${item.date} ${item.time}</p>
+          </div>
+        </div>`;
+  });
+  list.innerHTML = htmlStr;
+}
+
+addedBtn.addEventListener("click", function () {
+  listContent.unshift({
+    content: content.value,
+    date: date.value,
+    time: time.value,
+  });
+  render();
+});
+
+deletedBtn.addEventListener("click", function () {
+  listContent.shift();
+
+  render();
+});
